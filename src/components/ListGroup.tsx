7853c620f2bcebcb99c7578e4f11157eca857f78
ZidanCorson <zwang@scottlogic.com>
2025-12-01 11:44:53 +0000

    adds ListGroup

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,13 @@
 
 
 function ListGroup() {
-    const items = ['New York', 'San Francisco', 'Tokyo', 'Paris', 'London'];
-    
-    
+    let items = ['New York', 'San Francisco', 'Tokyo', 'Paris', 'London'];
+    items = [];
+
     return (
     <>
         <h1>List</h1>
+        {items.length === 0 && <p>No item found.</p>}
         <ul className="list-group">
         {items.map(item => (
             <li key={item}>{item}</li>
